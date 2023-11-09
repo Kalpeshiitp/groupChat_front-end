cc1e065b02cd5fffb26b363af9f7c6c22cd22b22
Kalpeshiitp <kalpeshkumar267@gmail.com>
2023-11-09 11:15:02 +0530

    crated get chat api

--- chat/chat.js
+++ chat/chat.js
@@ -1,21 +1,36 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const chatMessages = document.getElementById("chat-messages");
-    const messageInput = document.getElementById("message");
-    const sendButton = document.getElementById("send");
-  
-    sendButton.addEventListener("click", () => {
-      const messageText = messageInput.value;
-  
-      if (messageText.trim() !== "") {
-        appendMessage("You", messageText);
-        messageInput.value = "";
-      }
+async function fetchMessages() {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.get("http://localhost:3000/api/messages", {
+      headers: { Authorization: token },
     });
-  
-    function appendMessage(sender, message) {
+    const messages = response.data.messages;
+    const chatMessages = document.getElementById("chat-messages");
+    chatMessages.innerHTML = '';
+    messages.forEach((message) => {
       const messageDiv = document.createElement("div");
-      messageDiv.innerHTML = `<strong>${sender}:</strong> ${message}`;
+      messageDiv.innerHTML = `<strong>You:</strong> ${message.message}`;
       chatMessages.appendChild(messageDiv);
-    }
-  });
-  
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+async function postMsg() {
+  try {
+    const inputMsg = document.getElementById("message").value;
+    const inputMsgObj = {
+      message: inputMsg,
+    };
+    const token = localStorage.getItem("token");
+    await axios.post("http://localhost:3000/api/message", inputMsgObj, {
+      headers: { Authorization: token },
+    });
+    fetchMessages();
+    document.getElementById("message").value = "";
+  } catch (err) {
+    console.error(err);
+  }
+}
+document.getElementById("send").addEventListener("click", postMsg);
+window.addEventListener("load", fetchMessages);
